6d40c69a35ec75aa65c6f63f0c79fe92b47e44ce
Prakash Mathiyalagan <mathisprakash@gmail.com>
2024-11-17 22:56:23 +0000

    updated java spark code to invoke drools rules

--- src/main/java/com/data/SparkProcess.java
+++ src/main/java/com/data/SparkProcess.java
@@ -4,13 +4,16 @@ import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
-import org.drools.kiesession.rulebase.InternalKnowledgeBase;
-import org.drools.kiesession.rulebase.KnowledgeBaseFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
 import org.kie.api.io.Resource;
-import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.internal.builder.KnowledgeBuilder;
-import org.kie.internal.builder.KnowledgeBuilderFactory;
 import org.kie.internal.io.ResourceFactory;
 
 import java.util.Arrays;
@@ -34,20 +37,33 @@ public class SparkProcess {
         employee2.setSalary(9.00);
         Dataset<Employee> inputData = sparkSession.createDataset(Arrays.asList(employee1, employee2), Encoders.bean(Employee.class));
         inputData.show(false);
-        Resource resource = ResourceFactory.newClassPathResource("rules.drl");
-        KnowledgeBuilder knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        knowledgeBuilder.add(resource, ResourceType.DRL);
-        InternalKnowledgeBase internalKnowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
-        internalKnowledgeBase.addPackages(knowledgeBuilder.getKnowledgePackages());
-        Dataset<Employee> outputData = inputData.map(new MapFunction<Employee, Employee>() {
+        sparkSession.udf().register("ruleUdf", new RuleUdf(), DataTypes.StringType);
+
+        Dataset<Employee> output = inputData.map(new MapFunction<Employee, Employee>() {
             @Override
             public Employee call(Employee employee) throws Exception {
-                KieSession session = internalKnowledgeBase.newKieSession();
-                session.insert(employee);
-                session.fireAllRules();
+                KieServices kieServices = KieServices.Factory.get();
+                KieFileSystem kfs = kieServices.newKieFileSystem();
+                Resource res = ResourceFactory.newClassPathResource("rules.drl");
+                kfs.write(res);
+                KieBuilder kieBuilder = kieServices.newKieBuilder(kfs);
+                kieBuilder.buildAll();
+                Results results = kieBuilder.getResults();
+                if (results.hasMessages(Message.Level.ERROR)) {
+                    System.out.println(results.getMessages());
+                    throw new IllegalStateException("### errors ###");
+                }
+                KieContainer kieContainer =
+                        kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+
+                KieBase kieBase = kieContainer.getKieBase();
+                KieSession kieSession = kieBase.newKieSession();
+                kieSession.insert(employee);
+                kieSession.fireAllRules();
+
                 return employee;
             }
         }, Encoders.bean(Employee.class));
-        outputData.show(false);
+        output.show(false);
     }
 }
